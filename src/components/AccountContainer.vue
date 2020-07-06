349772e0700524e366978564eaed36ee619d7115
Aidan McKay <aidanmckay2000@gmail.com>
2020-07-06 15:37:15 -0500

    Add file to handle Account Container duties

---
+++ src/components/AccountContainer.vue
@@ -0,0 +1,17 @@
+<template>
+  <section class="grid" id="account-grid">
+    <section class="account-column grid" id="active-account-column">
+      <div class="account-container-title" id="active-account-container-title">
+        <h3>Active</h3>
+      </div>
+
+    </section>
+  </section>
+</template>
+
+<script>
+import AccountContent from './UserAccounts/AccountContent'
+export default {
+  name: 'AccountContainer'
+}
+</script>
