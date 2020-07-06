fbc6665035171d1a90aacbeb8a72f577f96dcc01
Aidan McKay <aidanmckay2000@gmail.com>
2020-07-06 15:15:42 -0500

    Refactor format for phone numbers and due dates

--- src/App.vue
+++ src/App.vue
@@ -14,7 +14,7 @@ export default {
   name: 'App',
   components: {
     Accounts
-  },
+  }, 
   data() {
     return {
       accounts: []
@@ -22,9 +22,31 @@ export default {
   },
   created() {
     axios.get('https://frontiercodingtests.azurewebsites.net/api/accounts/getall')
-    .then(res => this.accounts = res.data)
+    .then(res => res.data.map(account => {
+        let date = new Date(account.PaymentDueDate)
+        let newdate= (date.getMonth() + 1) + '/' + date.getDate() + '/' +  date.getFullYear();
+
+        // Would refactor and break this into a separate method with more time
+        let newPhone;
+        let formatPhoneNumber = (str) => {
+          let cleaned = ('' + str).replace(/\D/g, '');
+          let match = cleaned.match(/^(\d{3})(\d{3})(\d{4})$/);
+
+          if (match) {
+            newPhone = '(' + match[1] + ') ' + match[2] + '-' + match[3]
+            return newPhone
+          }
+
+          return null
+          };
+          formatPhoneNumber(account.PhoneNumber)
+          console.log(newPhone)
+        return {...account, newdate, newPhone}
+      })
+    )
+    .then(res => this.accounts = res)
     .catch(error => console.log(error))
-  }
+  },
 }
 
 </script>
--- src/components/UserAccounts/AccountContent.vue
+++ src/components/UserAccounts/AccountContent.vue
@@ -3,9 +3,9 @@
     <ul class="account-data-list">
       <li><label>Name: </label>{{account.LastName}}, {{account.FirstName}}</li>
       <li><label>Email: </label>{{account.Email}}</li>
-      <li><label>Phone Number: </label>{{account.PhoneNumber}}</li>
+      <li><label>Phone Number: </label>{{account.newPhone}}</li>
       <li><label>Amount Due: </label>${{account.AmountDue}}</li>
-      <li><label>Due Date: </label>{{account.DueDate}}</li>
+      <li><label>Due Date: </label>{{account.newdate}}</li>
     </ul>
   </div>
 </template>
